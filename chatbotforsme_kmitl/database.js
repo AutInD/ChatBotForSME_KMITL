7c8a6b52fd2f0de01e24c4c0ced4447c84510175
Nuttapong <devilkung52@gmail.com>
2021-09-22 15:15:58 +0700

    edit connect to SQL

--- chatbotforsme_kmitl/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-const dbConnection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "chatbotforsmedb"
-}).promise()
-
-module.exports = dbConnection;
--- chatbotforsme_kmitl/src/main.js
+++ chatbotforsme_kmitl/src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const mysql = require("mysql2");
+const dbConnection = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "chatbotforsmedb"
+}).promise()
+
+module.exports = dbConnection;
+
 createApp(App).use(store).use(router).mount('#app')
