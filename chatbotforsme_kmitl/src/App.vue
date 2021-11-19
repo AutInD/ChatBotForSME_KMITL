abab9dc89682c17c0e3033f1b913c0a7ac70816f
Nuttapong <devilkung52@gmail.com>
2021-11-19 15:39:09 +0700

    Frontend Product management

--- chatbotforsme_kmitl/src/App.vue
+++ chatbotforsme_kmitl/src/App.vue
@@ -13,11 +13,14 @@ import Navbar from '@/components/Navbar'
 
 
 
+
 export default {
   name: "App",
 
   components: {
     Navbar,
+   
+    
     
   },
 
--- chatbotforsme_kmitl/src/views/Home.vue
+++ chatbotforsme_kmitl/src/views/Home.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
   <v-app>
     <v-main class="main">
@@ -32,81 +33,15 @@
             Action
           </th>
           <th>
-            
-            <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          small absolute buttom right fab
-          color="red lighten-2"
-          dark
-          v-bind="attrs"
-          v-on="on"
-          
-        >
-        <v-icon>mdi-plus</v-icon>
-         
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title class="text-h5 grey lighten-2">
-          Add new items
-        </v-card-title>
-
-        <v-card-text>
-          <v-text-field
-                  label="ชื่อสินค้า"
-                  required
-                ></v-text-field>
-          <v-text-field
-                  label="จำนวน"
-                  required
-                ></v-text-field>
-          <v-text-field
-                  label="วันหมดอายุ"
-                  required
-                ></v-text-field>
-          <v-text-field
-                  label="ราคา"
-                  required
-                ></v-text-field>
-          <v-text-field
-                  label="รายละเอียด"
-                  required
-                ></v-text-field>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+              <Popup/>
+              
           </th>
           
         </tr>
         <!--<v-btn color="red" dark small absolute buttom right fab>
                     <v-icon>mdi-plus</v-icon>
                   </v-btn>-->
+                  
       </thead>
       <tbody>
         <tr
@@ -117,21 +52,20 @@
           <td>{{item.Product_Expire}}</td>
           <td>{{item.Product_Cost}}</td>
           <td>{{item.Product_Detail}}</td>
+          <td>
+
+          </td>
+          <!--{{item.Product_Picture}}-->
           <td> <v-img
-  contain
-  
-  max-height="150"
-  max-width="120"
-  src="https://th-test-11.slatic.net/p/a85fb067cd4f1b4b2e28bb1bb15597cd.jpg"
-></v-img></td>
+                  contain
+                  max-height="150"
+                  max-width="120"
+                  src="https://th-test-11.slatic.net/p/a85fb067cd4f1b4b2e28bb1bb15597cd.jpg"
+                ></v-img></td>
 
           <td>
-            <v-btn class="button"
-                color="yellow"
-                elevation="2"
-              >แก้ไข</v-btn>
-          
-            <v-btn class="button"
+           <upDate/>
+           <v-btn class="button"
                 color="red"
                 elevation="2"
                 v-on:click="deleteProduct(item.idProduct)"
@@ -153,11 +87,21 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Popup from '../components/popup_add_pd.vue'
+import upDate from '../components/popup_upd_pd.vue'
+import Swal from 'sweetalert2'
 
-Vue.use(VueAxios,axios)
+Vue.use(VueAxios,axios,Swal)
+
 
 export default {
+
   name: "home",
+  components: {
+    Popup,
+    upDate
+  },
+ 
   data() {
     
     return {
@@ -174,20 +118,40 @@ export default {
         this.products=result.data.data
       })
     },
+    
 
     deleteProduct(idProduct)
     {
-      confirm("ต้องการลบ ? ");
-      axios.delete('http://localhost:3000/api/product/'+idProduct).then(()=>{
+         //axios.delete('http://localhost:3000/api/product/'+idProduct).then(()=>{
+          Swal.fire({
+            title: 'ลบรายการสินค้า',
+            text: "คุณต้องการลบสินค้านี้หรือไม่ ?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'ยกเลิก',
+            confirmButtonText: 'ยืนยัน'
+        }).then((result)=>{
+          if (result.isConfirmed) {
+            Swal.fire(
+              'ลบ!',
+              'ลบสินค้าเรียบร้อยแล้ว',
+              'success'        
+              ).then(()=>{
+                axios.delete('http://localhost:3000/api/product/'+idProduct)
+                this.getData()
+              })
+            }       
+          this.getData()
+          
+        })
+      }
+    },
+    
 
-        this.getData()
 
-      })
-        
-      
-    }
-
-  },
+  
   
 
   mounted() {
@@ -197,11 +161,7 @@ export default {
 
   },
 
-  components: {
-
-
-  },
- 
+  
 
 
 };
